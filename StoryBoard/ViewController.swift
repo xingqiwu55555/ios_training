b56067ee0c019b25a4fcf997e9d051178dd84d3d
xingqiwu55555 <332016014@qq.com>
2022-08-03 16:51:32 +0800

    submit storyboard code

--- StoryBoard/ViewController.swift
+++ StoryBoard/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        self.button.setTitle("title1", for: .normal)
+        self.button.setTitle("login", for: .normal)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
