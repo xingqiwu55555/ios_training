a499a4453c95c5c7bf1f26db99d27fcc1b381844
xingqiwu55555 <332016014@qq.com>
2022-08-03 22:09:43 +0800

    add coding part two code

--- StoryBoard/CodingViewControllers.swift
+++ StoryBoard/CodingViewControllers.swift
@@ -67,12 +67,95 @@ class CodingViewController2: UIViewController {
         self.view.backgroundColor = .lightGray
 
     }
+    
+    func configTableView() {
+        
+    }
+}
+
+class CustomCell: UITableViewCell {
+    
+    var title = UILabel()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        let imageView = UIImageView(image: UIImage(named: "thoughtworks"))
+        imageView.contentMode = .scaleAspectFit
+        imageView.frame = CGRect(x: 20, y: 0, width: 60, height: 40)
+        self.contentView.addSubview(imageView)
+        
+
+        title.text = "cell title"
+        title.textColor = .black
+        title.frame = CGRect(x: 100, y: 0, width: self.contentView.frame.width - 100, height: 40)
+        self.contentView.addSubview(title)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
 
 class CodingViewController3: UIViewController {
+    
+    var tableData: [String] = [
+        "title1111",
+        "title2222",
+        "title13333",
+        "title114444",
+        "title115555"
+    ]
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
 
+        configTableView()
+    }
+    
+    func configTableView() {
+        let tableView = UITableView()
+        tableView.frame = CGRect(origin: .zero, size: self.view.frame.size)
+        self.view.addSubview(tableView)
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "UITableViewCell")
+        tableView.register(CustomCell.self, forCellReuseIdentifier: "CustomCell")
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+}
+
+extension CodingViewController3: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(#function, tableData[indexPath.row])
+        
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        print(#function)
+    }
+}
+
+extension CodingViewController3: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return 12
+        return tableData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell")!
+//        indexPath.section.row
+//        cell.textLabel?.text = "\(indexPath.row)"
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CustomCell") as! CustomCell
+        cell.title.text = tableData[indexPath.row]
+        
+        return cell
     }
 }
