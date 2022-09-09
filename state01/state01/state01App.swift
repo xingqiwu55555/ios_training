55e9892a80a875a7f44bd108916a5a7630de6b70
xingqiwu55555 <332016014@qq.com>
2022-09-09 11:01:46 +0800

    add 3-state01

---
+++ state01/state01/state01App.swift
@@ -0,0 +1,17 @@
+//
+//  state01App.swift
+//  state01
+//
+//  Created by xmshen on 2022/9/9.
+//
+
+import SwiftUI
+
+@main
+struct state01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
